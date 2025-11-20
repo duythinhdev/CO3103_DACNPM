4d90998a3e6f2ab617e188749c9b117d5548b6bc
duythinhdev <do.duythinh1@gmail.com>
2025-11-21 06:20:56 +0700

    refactoring code

--- api/index.js
+++ api/index.js
@@ -2,25 +2,21 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const { WebSocketServer } = require("ws");
 const Message = require('./models/Message');
-const ws = require('ws');
-const fs = require('fs');
 const userRouter = require('../api/router/user/index');
 const groupRouter = require('../api/router/group/index');
+const WebSocketManager = require('./wss/WebSocketManager');
 const { getUserDataFromRequest } = require('../api/util/index');
 const { connectionDb } = require('../api/mongodb/index');
 require('./util/passport');
 
 dotenv.config();
 
-const jwtSecret = process.env.JWT_SECRET;
-
 const app = express();
 connectionDb();
 app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
-
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -28,12 +24,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   res.setHeader("Referrer-Policy", "no-referrer");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  // res.header(
+  //     "Access-Control-Allow-Headers",
+  //     "Origin, X-Requested-With, Content-Type, Accept"
+  // );
   next();
 });
 
@@ -64,96 +60,6 @@ const server = app.listen(PORT, () => {
     process.exit(1);
   }
 });
+const wss = new WebSocketServer({ server });
 
-const wss = new ws.WebSocketServer({ server });
-let clientConns = {};
-wss.on('connection', (connection, req) => {
-  function notifyAboutOnlinePeople() {
-    for (const [userId, conObject] of Object.entries(clientConns)) {
-      conObject.client.send(
-        JSON.stringify({
-          online: Object.entries(clientConns).map(([userId, conObject]) => ({
-            userId: userId,
-            username: conObject.username,
-          })),
-        }),
-      );
-    }
-  }
-
-  connection.isAlive = true;
-
-  connection.timer = setInterval(() => {
-    connection.ping();
-    connection.deathTimer = setTimeout(() => {
-      connection.isAlive = false;
-      clearInterval(connection.timer);
-      connection.terminate();
-      notifyAboutOnlinePeople();
-      console.log("dead");
-    }, 1000);
-  }, 5000);
-
-  connection.on("pong", () => {
-    clearTimeout(connection.deathTimer);
-  });
-
-  // read username and id form the cookie for this connection
-  const cookies = req.headers.cookie;
-  if (cookies) {
-    const tokenCookieString = cookies.split("; ").find((str) => str.startsWith("token="));
-    if (tokenCookieString) {
-      const token = tokenCookieString.split("=")[1];
-      if (token) {
-        jwt.verify(token, jwtSecret, {}, (err, userData) => {
-          if (err) throw err;
-          const { userId, username } = userData;
-          clientConns[userId] = {
-            username: username,
-            client: connection,
-          };
-          connection.userId = userId;
-          connection.username = username;
-        });
-      }
-    }
-  }
-
-  connection.on("message", async (message) => {
-    const messageData = JSON.parse(message.toString());
-    const { recipient, text, file } = messageData;
-    let filename = null;
-    if (file) {
-      console.log("size", file.data.length);
-      const parts = file.name.split(".");
-      const ext = parts[parts.length - 1];
-      filename = Date.now() + "." + ext;
-      const path = __dirname + "/uploads/" + filename;
-      const bufferData = new Buffer(file.data.split(",")[1], "base64");
-      fs.writeFile(path, bufferData, () => {
-        console.log("file saved:" + path);
-      });
-    }
-
-    if (recipient && (text || file)) {
-      const messageDoc = await Message.create({
-        type: "user",
-        sender: connection.userId,
-        recipient,
-        text,
-        file: file ? filename : null,
-      });
-
-      clientConns[recipient]?.client?.send(
-        JSON.stringify({
-          text,
-          sender: connection.userId,
-          recipient,
-          file: file ? filename : null,
-          _id: messageDoc._id,
-        }),
-      );
-    }
-  });
-  notifyAboutOnlinePeople();
-});
+new WebSocketManager(wss);
---
+++ api/wss/WebSocketManager.js
@@ -0,0 +1,114 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const Message = require('../models/Message');
+
+class WebSocketManager {
+    constructor(wss) {
+        this.wss = wss;
+        this.clientConns = {};
+
+        this.wss.on('connection', (connection, req) => {
+            this.handleConnection(connection, req);
+        });
+    }
+
+    notifyAboutOnlinePeople() {
+        const online = Object.entries(this.clientConns).map(([userId, c]) => ({
+            userId,
+            username: c.username,
+        }));
+
+        for (const [userId, conObj] of Object.entries(this.clientConns)) {
+            conObj.client.send(JSON.stringify({ online }));
+        }
+    }
+
+    handleConnection(connection, req) {
+        connection.isAlive = true;
+
+        connection.timer = setInterval(() => {
+            connection.ping();
+            connection.deathTimer = setTimeout(() => {
+                connection.isAlive = false;
+                clearInterval(connection.timer);
+                connection.terminate();
+                delete this.clientConns[connection.userId];
+                this.notifyAboutOnlinePeople();
+            }, 1000);
+        }, 5000);
+
+        connection.on("pong", () => {
+            clearTimeout(connection.deathTimer);
+        });
+
+        this.handleAuth(connection, req);
+
+        connection.on("message", (message) => {
+            this.handleMessage(connection, message);
+        });
+
+        this.notifyAboutOnlinePeople();
+    }
+
+    handleAuth(connection, req) {
+        const cookies = req.headers.cookie;
+        if (!cookies) return;
+
+        const tokenCookieString = cookies.split("; ").find((str) => str.startsWith("token="));
+        if (!tokenCookieString) return;
+
+        const token = tokenCookieString.split("=")[1];
+        if (!token) return;
+
+        jwt.verify(token, process.env.JWT_SECRET, {}, (err, userData) => {
+            if (err) throw err;
+
+            const { userId, username } = userData;
+
+            this.clientConns[userId] = {
+                username,
+                client: connection,
+            };
+
+            connection.userId = userId;
+            connection.username = username;
+        });
+    }
+
+    async handleMessage(connection, message) {
+        const { recipient, text, file } = JSON.parse(message);
+
+        let filename = null;
+
+        if (file) {
+            const ext = file.name.split(".").pop();
+            filename = `${Date.now()}.${ext}`;
+            const path = __dirname + "/uploads/" + filename;
+
+            const bufferData = Buffer.from(file.data.split(",")[1], "base64");
+            fs.writeFileSync(path, bufferData);
+        }
+
+        if (recipient && (text || file)) {
+            const messageDoc = await Message.create({
+                type: "user",
+                sender: connection.userId,
+                recipient,
+                text,
+                file: filename,
+            });
+
+            this.clientConns[recipient]?.client?.send(
+                JSON.stringify({
+                    text,
+                    sender: connection.userId,
+                    recipient,
+                    file: filename,
+                    _id: messageDoc._id,
+                }),
+            );
+        }
+    }
+}
+
+module.exports = WebSocketManager;
